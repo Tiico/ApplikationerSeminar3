8768378ced4f0ebdc5a33dff096edb6d6abc6644
tiico <ockelberg@gmail.com>
2017-12-02 17:31:41 +0100

    Added validation and popup for submitting comments

--- application/controllers/Comments.php
+++ application/controllers/Comments.php
@@ -2,10 +2,19 @@
 class Comments extends CI_Controller{
     public function create(){
         $food = $this->input->post('food');
+        $this->form_validation->set_rules('body', 'Comment', 'trim|required');
+        $data['comments'] = $this->comments_model->get_comments();
 
-        $this->comments_model->create_comment($food);
+        if($this->form_validation->run() === FALSE){
+            $this->load->view('templates/header');
+            $this->load->view('pages/'.$food, $data);
+            $this->load->view('templates/footer');
 
-        $this->session->set_flashdata('comment_created', 'Your comment has been created!');
+            $this->session->set_flashdata('comment_created_fail', 'Wrong comment format, try again!!');
+        }else{
+            $this->comments_model->create_comment($food);
+            $this->session->set_flashdata('comment_created', 'Your comment has been created!');
+        }
         redirect($food);
     }
     public function delete($id){
@@ -16,5 +25,3 @@ class Comments extends CI_Controller{
         redirect($food);
     }
 }
-
-?>
