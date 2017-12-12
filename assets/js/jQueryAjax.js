a1564161790d5efa780bcc94fea907b032aeb46a
tiico <ockelberg@gmail.com>
2017-12-12 01:47:43 +0100

    Javascript for handling comments, external.

---
+++ assets/js/jQueryAjax.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    showComments();
+    $('#addcomment').click(function(){
+        var url = $('#myForm').attr('action');
+        var data = $('#myForm').serialize();
+
+        $.ajax({
+            type: 'ajax',
+            method: 'post',
+            url: url,
+            data: data,
+            async: false,
+            dataType: 'json',
+            success: function(response){
+                if(response.success){
+                    $('#myForm')[0].reset();
+                    $('.alert-success').html('Your comment has been posted!').fadeIn().delay(4000).fadeOut('slow');
+                    showComments();
+                }else{
+                    alert('Error');
+                }
+            },
+            error: function(){
+                alert('Could not comment');
+            }
+        });
+    })
+
+    $('#test').on('click', '.deletebutton', function(){
+        var id = $(this).attr('data');
+        $.ajax({
+            type: 'ajax',
+            method: 'get',
+            async: false,
+            url: 'http://localhost/seminarie3/comments/deletee',
+            data:{id:id},
+            dataType: 'json',
+            success: function(response){
+                if(response.success){
+                    $('.alert-success').html('Comment Deleted successfully').fadeIn().delay(4000).fadeOut('slow');
+                    showComments();
+                }else{
+                    alert('Error');
+                }
+            },
+            error: function(){
+                alert('Error deleting');
+            }
+        });
+    });
+
+
+    function showComments(){
+        $.ajax({
+            type: 'ajax',
+            url: 'http://localhost/seminarie3/Comments/showcomments',
+            async: false,
+            dataType: 'json',
+            success: function(data){
+                var output = '';
+                var deletee = '';
+                var i;
+                for(i = 0; i<data.length;i++){
+                    if($('#username').text() == data[i].username){
+                        deletee = '<a href="javascript:;" class="deletebutton" data="'+data[i].id+'">Delete</a>'
+                    }else{
+                        deletee = '';
+                    }
+                    if(data[i].food == 'meatballs'){
+                        output += '<div class="comment">'+deletee+'<h3 class="commentusername">'+data[i].username+'</h3><p>'+data[i].comment+'</p></div>';
+                    }
+                }
+
+                $('#test').html(output);
+            },
+            error: function(){
+                alert('could not get data from database');
+            }
+        })
+    }
+})
