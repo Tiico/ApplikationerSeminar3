a7899b115acbb40b31d4785794858105c0d01904
tiico <ockelberg@gmail.com>
2017-12-12 01:44:58 +0100

    Added Javascript comment model

--- application/models/comments_model.php
+++ application/models/comments_model.php
@@ -3,7 +3,47 @@ class comments_model extends CI_Model{
     public function __construct(){
         $this->load->Database();
     }
-    public function get_comments($slug = FALSE){
+    public function showComments(){
+        $this->db->order_by('id', 'DESC');
+        $query = $this->db->get('comments');
+        if($query->num_rows() > 0){
+            return $query->result();
+        }else{
+            return false;
+        }
+    }
+    public function addComment(){
+        $comment = htmlspecialchars($this->input->post('body'));
+        $data = array(
+            'username' => $this->session->userdata('username'),
+            'comment' => $comment,
+            'food'=>$this->input->post('food'),
+        );
+        $this->db->insert('comments', $data);
+        if($this->db->affected_rows() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    function deletee_comment(){
+        $id = $this->input->get('id');
+        $comment_query = $this->db->query("SELECT * FROM comments WHERE id = '$id'");
+        if($comment_query->row(0)->username == $this->session->userdata('username')){
+            $this->db->query("DELETE FROM comments WHERE id = '$id'");
+            if($this->db->affected_rows() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+
+}
+/********************** Without Javascript *********************/
+
+/*    public function get_comments($slug = FALSE){
         if($slug === FALSE){
             $this->db->order_by('id', 'DESC');
             $query = $this->db->get('comments');
@@ -13,15 +53,15 @@ class comments_model extends CI_Model{
         return $query->row_array();
     }
     public function create_comment($food){
-            $test = htmlspecialchars($this->input->post('body'));
-            $data = array(
-                'food' => $food,
-                'username' => $this->session->userdata('username'),
-                'comment' => $test
-                );
+        $comment = htmlspecialchars($this->input->post('body'));
+        $data = array(
+            'food' => $food,
+            'username' => $this->session->userdata('username'),
+            'comment' => $comment
+        );
 
-            $this->db->insert('comments', $data);
-        }
+        $this->db->insert('comments', $data);
+    }
     public function delete_comment($id){
         $sql = ("SELECT * FROM comments WHERE id = ?");
         $comment_query = $this->db->query($sql, array($id));
@@ -32,5 +72,4 @@ class comments_model extends CI_Model{
         }else{
             die('You cant delete other users comments!');
         }
-    }
-}
+    }*/
